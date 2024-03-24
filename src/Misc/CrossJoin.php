85c9d07d3a3256c894fac8896879356b7862b30a
Maxim Linenko <linenko.maxim@gmail.com>
2024-03-24 13:06:50 +0200

    Algo: Arrays CrossJoin

---
+++ src/Misc/CrossJoin.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Misc;
+
+class CrossJoin
+{
+    public static function join(...$arrays): array
+    {
+        $results = [[]];
+
+        foreach ($arrays as $index => $array) {
+            $append = [];
+
+            foreach ($results as $product) {
+                foreach ($array as $item) {
+                    $product[$index] = $item;
+
+                    $append[] = $product;
+                }
+            }
+
+            $results = $append;
+        }
+
+        return $results;
+    }
+}
---
+++ tests/Misc/CrossJoinTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Tests\Misc;
+
+use MaxLZp\Algo\Misc\CrossJoin;
+use PHPUnit\Framework\TestCase;
+
+class CrossJoinTest extends TestCase
+{
+    public function testShouldJoin()
+    {
+        $result = CrossJoin::join([0, 1], [2], [3, 4]);
+        $this->assertTrue(true);
+    }
+}
