7b56a69dc5f9a0cdb72eabea64bce98f5cadbf54
Maxim Linenko <linenko.maxim@gmail.com>
2024-04-18 09:58:45 +0300

    Algo. Misc. Prime numbers

---
+++ src/Misc/PrimeNumber/PrimeNumber.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Misc\PrimeNumber;
+
+final class PrimeNumber
+{
+    public static function isPrime(int $number): bool
+    {
+        if ($number < 2) { return false; }
+        for ($i = 2, $max = sqrt($number); $i <= $max; $i++) {
+            if ($number % $i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static function findPrimes(int $min, int $max): array
+    {
+        $ceive = [];
+        $numbers = [];
+        for ($key = max(2, $min); $key <= $max; $key++) {
+            if (isset($ceive[$key])) { continue; }
+            if (self::isPrime($key)) { $numbers[] = $key; }
+            for ($i = 2 * $key; $i <= $max; $i += $key) {
+                $ceive[$i] = $i;
+            }
+        }
+        return $numbers;
+    }
+}
---
+++ tests/Misc/PrimeNumber/PrimeNumberTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Tests\Misc\PrimeNumber;
+
+use MaxLZp\Algo\Misc\PrimeNumber\PrimeNumber;
+use PHPUnit\Framework\TestCase;
+
+final class PrimeNumberTest extends TestCase
+{
+    /**
+     * @dataProvider isPrimeProvider
+     *
+     * @param  integer $number
+     * @param  boolean $expected
+     * @return void
+     */
+    public function test_should_determine_if_prime_or_not(int $number, bool $expected): void
+    {
+        $this->assertEquals($expected, PrimeNumber::isPrime($number));
+    }
+
+    public function isPrimeProvider(): array
+    {
+        return [
+            [1, false],
+            [2, true],
+            [3, true],
+            [4, false],
+            [5, true],
+            [10, false],
+        ];
+    }
+
+    /**
+     * @dataProvider findPrimesProvider
+     *
+     * @param  integer $min
+     * @param  integer $max
+     * @param  array   $expected
+     * @return void
+     */
+    public function test_should_find_primes_in_range(int $min, int $max, array $expected): void
+    {
+        $this->assertSame($expected, PrimeNumber::findPrimes($min, $max));
+    }
+
+    public function findPrimesProvider(): array
+    {
+        return [
+            [0, 1, []],
+            [0, 2, [2]],
+            [2, 4, [2, 3]],
+            [0, 10, [2, 3, 5, 7]],
+            [-5, 10, [2, 3, 5, 7]],
+        ];
+    }
+}
