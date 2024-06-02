bba9e5b4e53a122c5c485d22ef2ed90951281101
Maxim Linenko <linenko.maxim@gmail.com>
2024-06-02 07:33:21 +0300

    Algo. Sort. Merge Sort

---
+++ tests/Sorting/MergeSortTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Tests\Sorting;
+
+use MaxLZp\Algo\Sorting\MergeSort as SortingMergeSort;
+use MaxLZp\Algo\Sorting\SortDirection;
+use PHPUnit\Framework\TestCase;
+
+class MergeSortTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider sortingProviderAsc
+     * @param  array $input
+     * @param  array $expected
+     * @return void
+     */
+    public function shouldSortAsc($input, $expected): void
+    {
+        $result = SortingMergeSort::sort($input);
+        $this->assertSame($expected, $result);
+    }
+
+    public function sortingProviderAsc(): array
+    {
+        return [
+            [
+                [1,2,3,4,5],
+                [1,2,3,4,5],
+            ],
+            [
+                [1,3,2,5,4],
+                [1,2,3,4,5],
+            ],
+            [
+                [5,4,3,2,1],
+                [1,2,3,4,5],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider sortingProviderDesc
+     * @param  array $input
+     * @param  array $expected
+     * @return void
+     */
+    public function shouldSortDesc($input, $expected): void
+    {
+        $result = SortingMergeSort::sort($input, SortDirection::DESC);
+        $this->assertSame($expected, $result);
+    }
+
+    public function sortingProviderDesc(): array
+    {
+        return [
+            [
+                [1,2,3,4,5],
+                [5,4,3,2,1],
+            ],
+            [
+                [1,3,2,5,4],
+                [5,4,3,2,1],
+            ],
+            [
+                [5,4,3,2,1],
+                [5,4,3,2,1],
+            ],
+        ];
+    }
+}
