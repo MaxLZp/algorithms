50cb963b1a505e09d0bd17a41413f9ffb921c4db
Maxim Linenko <linenko.maxim@gmail.com>
2024-06-05 08:49:18 +0300

    Algo. Recursion. Print mathematical table of n

---
+++ tests/Recursion/PrintTableRecursivelyTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Tests\Recursion;
+
+use MaxLZp\Algo\Recursion\PrintTableRecursively;
+use PHPUnit\Framework\TestCase;
+
+final class PrintTableRecursivelyTest extends TestCase
+{
+    /**
+     * @test
+     * @param  integer $n
+     * @return void
+     */
+    public function shouldPrintTableNonRecursively(): void
+    {
+        $this->doesNotPerformAssertions();
+
+        echo PHP_EOL.'NonRecursive:'.PHP_EOL;
+        PrintTableRecursively::printNonRecursive(10);
+        echo PHP_EOL.'Recursive:'.PHP_EOL;
+        PrintTableRecursively::printRecursive(10);
+    }
+}
