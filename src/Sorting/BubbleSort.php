37d33f35a31cf946da53599dfd28de71192a2f54
Maxim Linenko <linenko.maxim@gmail.com>
2024-07-29 07:47:27 +0300

    Improve bubble sort

--- src/Sorting/BubbleSort.php
+++ src/Sorting/BubbleSort.php
@@ -15,7 +15,7 @@ class BubbleSort
         $comparer = $direction->getComparer();
 
         for ($i = 0; $i < count($input); $i++) {
-            for ($j = 1; $j < count($input); $j++) {
+            for ($j = 1; $j < count($input) - $i; $j++) {
                 if ($comparer($input[$j - 1], $input[$j])) {
                     [$input[$j - 1], $input[$j]] = [$input[$j], $input[$j - 1]];
                 }
