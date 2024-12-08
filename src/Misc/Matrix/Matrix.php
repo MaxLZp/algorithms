a4680c9f0e98aa39dd830f426dfca34a4e7709a3
Maxim Linenko <linenko.maxim@gmail.com>
2024-12-08 18:48:19 +0200

    Algo. Misc. Multiply matrices

---
+++ src/Misc/Matrix/Matrix.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Misc\Matrix;
+
+class Matrix
+{
+    /**
+     * Multiply matrices
+     *
+     * @param array $a
+     * @param array $b
+     * @return array
+     */
+    public static function multiply($a, $b): array
+    {
+        $result = [];
+        for ($i=0; $i < count($a); $i++) {
+            for ($j=0; $j < count($a); $j++) {
+                $result[$i][$j] = 0;
+            }
+        }
+
+        for($row = 0; $row < count($result); $row++) {
+            for($col = 0; $col < count($result); $col++) {
+
+                for($c = 0; $c < count($b); $c++) {
+                    $result[$row][$col] += $a[$row][$c] * $b[$c][$col];
+                }
+
+            }
+        }
+
+
+        return $result;
+    }
+}
---
+++ tests/Misc/MatrixMultilpyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MaxLZp\Algo\Tests\Misc;
+
+use PHPUnit\Framework\TestCase;
+use MaxLZp\Algo\Misc\Matrix\Matrix;
+
+class MatrixMultilpyTest extends TestCase
+{
+    public function testMultiply(): void
+    {
+        $this->assertSame(
+            [
+                [28, 34],
+                [64, 79],
+            ],
+            Matrix::multiply(
+                [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                ], [
+                    [2, 3],
+                    [4, 5],
+                    [6, 7],
+                ]
+            )
+        );
+    }
+}
